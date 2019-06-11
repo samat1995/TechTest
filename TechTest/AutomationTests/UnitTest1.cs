253f87f3897c3d2205f9fc585273b24f45a5db95
mrufcia98@gmail.com <mrufcia98@gmail.com>
2019-06-11 22:08:56 +0100

    added delete post test

--- TechTest/AutomationTests/UnitTest1.cs
+++ TechTest/AutomationTests/UnitTest1.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Threading.Tasks;
 using AutomationTests.Services;
-using AutomationTests.Types;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
 using FluentAssertions;
+using System.Net;
 
 namespace AutomationTests
 {
@@ -20,11 +20,9 @@ namespace AutomationTests
 
 
         [TestMethod]
-        public async Task CreatedPostTests()
+        public async Task CreatePostTest()
         {
             // arrange
-
-            var postsPreCreation = await _jsonPlaceholderService.GetAllPostsAsync();
             var newPost = new
             {
                 Title = Guid.NewGuid().ToString("N"),
@@ -42,6 +40,23 @@ namespace AutomationTests
             retrievedPost.Title.Should().Be(newPost.Title);
             retrievedPost.Body.Should().Be(newPost.Body);
             retrievedPost.UserId.Should().Be(newPost.UserId);
+        }
+
+
+        [TestMethod]
+        public async Task DeletePostTest()
+        {
+            // arrange
+            var postsPreCreation = await _jsonPlaceholderService.GetAllPostsAsync();
+            var postToDelete = postsPreCreation.First();
+
+            // act
+            var response = await _jsonPlaceholderService.DeletePost(postToDelete.Id);
+
+            // assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var retrievedPost = await _jsonPlaceholderService.GetPostAsync(postToDelete.Id);
+            retrievedPost.Should().BeNull();
         }
     }
 }
